796f46767e3d924d52e40513faae8f31ec81279d
SpencerSolomon <spencersolomon6@gmail.com>
2023-04-16 05:02:32 -0400

    Working implementation for the Encoder-Decoder model

--- main.py
+++ main.py
@@ -1,36 +1,37 @@
 import os
 
 import gensim.downloader
-from nltk.translate.bleu_score import sentence_bleu
+import torch.cuda
+from nltk.translate.bleu_score import sentence_bleu, SmoothingFunction
 import numpy as np
 import pandas as pd
-from gensim.models import KeyedVectors
+from gensim.models import KeyedVectors, Word2Vec
 from gensim.summarization.summarizer import summarize
 from rouge_score.rouge_scorer import RougeScorer
 from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
+from nltk.tokenize import word_tokenize, RegexpTokenizer
 import matplotlib.pyplot as plt
 from seq2seq import Seq2Seq
 from collections import Counter
+from typing import List
 
 MODEL_NAME = "pretrained.model"
 
 
-def preprocess_data(data: pd.Series, vector_model: KeyedVectors):
+def preprocess_data(data: pd.Series) -> List[List[str]]:
     """
     Transforms a Series of data into a list of list of word vectors for each document
 
     :param data:
     :return: list of list of vectors
     """
+    data = data.str.lower()
+    clean_texts = []
+    for i, text in data.items():
+        remove_punct = RegexpTokenizer(r'\w+')
+        clean_texts.append(remove_punct.tokenize(text))
 
-    vectors = []
-    for i, text in data.iteritems():
-        tokenized_text = [word for word in word_tokenize(text)]
-
-        vectors.append([vector_model[word] if word in vector_model.key_to_index else np.zeros(300) for word in tokenized_text])
-
-    return vectors
+    return clean_texts
 
 
 def rogue_score(predictions, references, scorer: RougeScorer):
@@ -45,11 +46,14 @@ def rogue_score(predictions, references, scorer: RougeScorer):
 
     scores = pd.DataFrame(columns=['prediction', 'reference', 'precision', 'recall', 'fmeasure'])
     for predicted, reference in zip(predictions, references):
-        score = scorer.score(predicted, reference)['rouge2']
-        score_dict = {'prediction': predicted, 'reference': reference, 'precision': score.precision,
-                      'recall': score.recall, 'fmeasure': score.fmeasure}
+        predicted = ' '.join(predicted)
+        reference = ' '.join(reference)
 
-        scores = scores.append(score_dict, ignore_index=True)
+        score = scorer.score(predicted, reference)['rouge2']
+        score_dict = pd.DataFrame([{'prediction': predicted, 'reference': reference, 'precision': score.precision,
+                                    'recall': score.recall, 'fmeasure': score.fmeasure}])
+
+        scores = pd.concat([scores, score_dict], ignore_index=True)
 
     return scores
 
@@ -57,13 +61,11 @@ def rogue_score(predictions, references, scorer: RougeScorer):
 def bleu_score(predictions, references):
 
     scores = pd.DataFrame(columns=['prediction', 'reference', 'bleu_score'])
+    smooth = SmoothingFunction()
     for prediction, reference in zip(predictions, references):
-        tok_prediction = word_tokenize(prediction)
-        tok_reference = word_tokenize(reference)
-
-        score = sentence_bleu(tok_reference, tok_prediction)
-        score_dict = {"prediction": prediction, "reference": reference, "bleu_score": score}
-        scores = scores.append(score_dict, ignore_index=True)
+        score = sentence_bleu(reference, prediction, smoothing_function=smooth.method3)
+        score_dict = pd.DataFrame([{"prediction": ' '.join(prediction), "reference": ' '.join(reference), "bleu_score": score}])
+        scores = pd.concat([scores, score_dict], ignore_index=True)
 
     return scores
 
@@ -112,42 +114,49 @@ def print_results(scores, model_name):
 
 
 if __name__ == '__main__':
+    print("Beginning Encoder-Decoder Model Training")
+
     train = pd.read_csv("data/train.csv")
     validation = pd.read_csv("data/validation.csv")
     test = pd.read_csv("data/test.csv")
 
-    vocab = []
-    vocab_counts = Counter()
-    threshold = 5
-    for i, row in train.iterrows():
-        vocab_counts.update(row['article'].split())
+    training_inputs = preprocess_data(train["article"][100:])
+    training_references = preprocess_data(train["highlights"][100:])
+    validation_inputs = preprocess_data(validation["article"][100:])
+    validation_references = preprocess_data(validation["article"][100:])
 
-    for word, occurence_count in vocab_counts.items():
-        if occurence_count >= threshold:
-            vocab.append(word)
+    vector_model = Word2Vec(sentences=training_inputs + training_references, size=300, window=5, min_count=1, workers=4, sg=1)
+    vector_model.save("original_w2v.npy")
+    #vector_model = Word2Vec.load("original_w2v.npy")
 
-    if os.path.exists(MODEL_NAME):
-        vector_model = KeyedVectors.load(MODEL_NAME)
-    else:
-        vector_model = gensim.downloader.load('fasttext-wiki-news-subwords-300')
-        vector_model.save(MODEL_NAME)
+    print("Trained Word2Vec Model... Beginning Training")
 
-    # print(preprocess_data(train.head()["article"], vector_model))
+
     scorer = RougeScorer(["rouge2"], use_stemmer=True)
-    baseline_scores = perform_baseline(test, scorer)
-    print_results(baseline_scores, "Baseline Model")
+    # baseline_scores = perform_baseline(test, scorer)
+    # print_results(baseline_scores, "Baseline Model")
 
-    seq2seq_model = Seq2Seq(256, 100, vocab, .01)
+    seq2seq_model = Seq2Seq(300, 100, vector_model, .01)
 
-    inputs = train['article'].values.tolist()
-    references = train['highlights'].values.tolist()
-    val_inputs = validation['article'].values.tolist()
+    losses = seq2seq_model.train_model(training_inputs, training_references, epochs=10)
+    seq2seq_model.save()
 
-    seq2seq_model.train_model(inputs, references, epochs=10)
-    results = seq2seq_model.predict(val_inputs)
-    rouge_scores = rogue_score(results, references, scorer)
-    bleu_scores = bleu_score(results, references)
+    print("Done Training model, Starting Evaluation:")
 
-    seq2seq_scores = pd.merge(rouge_scores, bleu_scores, on=['prediction', 'reference'])
+    results = seq2seq_model.predict(validation_references)
+    rouge_scores = rogue_score(results, validation_references, scorer)
+    bleu_scores = bleu_score(results, validation_references)
+
+    seq2seq_scores = pd.merge(rouge_scores, bleu_scores, on='reference')
     print_results(seq2seq_scores, "Seq2Seq Model")
 
+    seq2seq_scores.to_csv("validation_scores.csv")
+    print(seq2seq_scores)
+
+    plt.plot(range(len(losses)), losses)
+    plt.xlabel("Epoch")
+    plt.ylabel("Loss")
+    plt.title("Encoder-Decoder Training Loss")
+    plt.show()
+
+    plt.savefig("Loss Chart")
--- seq2seq.py
+++ seq2seq.py
@@ -1,10 +1,69 @@
+from collections import Counter
 from typing import List
 from base_model import BaseModel
 from nltk.tokenize import word_tokenize
 from torch.nn import LSTM, Embedding, Module, Linear, Softmax, CrossEntropyLoss
-from torch.optim import RMSprop
+from torch.optim import RMSprop, Adam
 import torch
 import numpy as np
+from gensim.models import KeyedVectors, Word2Vec
+import random
+
+
+class Encoder(Module):
+    embedding_size: int
+    hidden_size: int
+
+    lstm: LSTM
+
+    def __init__(self, embedding_size, hidden_size):
+        super().__init__()
+
+        self.embedding_size = embedding_size
+        self.hidden_size = hidden_size
+
+        self.init_model()
+
+    def init_model(self):
+        self.lstm = LSTM(self.embedding_size, self.hidden_size, num_layers=1, dropout=.4)
+
+    def forward(self, x, hidden=None):
+        if hidden is not None:
+            return self.lstm(x, hidden)
+        else:
+            return self.lstm(x)
+
+    def init_hidden(self):
+        return [torch.rand(1, self.hidden_size) for i in range(2)]
+
+
+class Decoder(Module):
+    vocab_size: int
+    hidden_size: int
+
+    lstm: LSTM
+    linear: Linear
+    activation: Softmax
+
+    def __init__(self, vocab_size, hidden_size):
+        super().__init__()
+
+        self.vocab_size = vocab_size
+        self.hidden_size = hidden_size
+
+        self.init_model()
+
+    def forward(self, x, hidden):
+        cn, hn = hidden
+
+        lstm_out, hidden = self.lstm(x, (cn, hn))
+        linear_out = self.linear(lstm_out[0])
+        return self.activation(linear_out), hidden
+
+    def init_model(self):
+        self.lstm = LSTM(1, self.hidden_size, dropout=.4)
+        self.linear = Linear(self.hidden_size, self.vocab_size)
+        self.activation = Softmax(dim=0)
 
 
 class Seq2Seq(Module, BaseModel):
@@ -13,89 +72,124 @@ class Seq2Seq(Module, BaseModel):
     embedding_size: int
     vocab_size: int
     hidden_size: int
-    vocabulary: list
+    max_length: int = 50
 
-    enc_embedding: Embedding
-    enc_lstm1: LSTM
-    enc_lstm2: LSTM
-    enc_lstm3: LSTM
-    dec_embedding: Embedding
-    dec_lstm: LSTM
-    fc_linear: Linear
-    output_layer: Softmax
+    encoder: Encoder
+    decoder: Decoder
+    vector_model: Word2Vec
 
-    def __init__(self, embedding_size, hidden_size, vocabulary, learning_rate=.01):
+    def __init__(self, embedding_size, hidden_size, vector_model, learning_rate=.01):
         super().__init__()
         self.learning_rate = learning_rate
 
         self.embedding_size = embedding_size
-        self.vocab_size = len(vocabulary)
         self.hidden_size = hidden_size
-        self.vocabulary = vocabulary
+        self.vector_model = vector_model
+        self.vocab_size = len(vector_model.wv.index2word) + 2
 
         self.init_model()
 
     def init_model(self):
-        self.enc_embedding = Embedding(self.vocab_size, self.embedding_size)
+        self.encoder = Encoder(self.embedding_size, self.hidden_size)
+        self.decoder = Decoder(self.vocab_size, self.hidden_size)
 
-        self.enc_lstm1 = LSTM(self.embedding_size, self.hidden_size, dropout=.4)
-        self.enc_lstm2 = LSTM(self.hidden_size, self.hidden_size, dropout=.4)
-        self.enc_lstm3 = LSTM(self.hidden_size, self.hidden_size, dropout=.4)
+    def sentence2tensor(self, sentence: List[str]):
+        vector = [self.vector_model.wv[word] for word in sentence if word in self.vector_model.wv]
+        return torch.tensor(np.array(vector), dtype=torch.float)
 
-        self.dec_embedding = Embedding(self.vocab_size, self.embedding_size)
+    def train_model(self, train_data: List[List[str]], references: List[List[str]], epochs: int = 10) -> List[float]:
+        loss_criteria = CrossEntropyLoss()
+        encoder_optimizer = Adam(self.encoder.parameters(), lr=self.learning_rate)
+        decoder_optimizer = Adam(self.decoder.parameters(), lr=self.learning_rate)
 
-        self.dec_lstm = LSTM(self.embedding_size, self.hidden_size, dropout=.4)
-        self.fc_linear = Linear(self.hidden_size, self.vocab_size)
-        self.output_layer = Softmax()
-
-    def forward(self, article):
-        enc_embeddings = self.enc_embedding(article)
-
-        enc_lstm1_out, _ = self.enc_lstm1(enc_embeddings)
-        enc_lstm2_out, _ = self.enc_lstm2(enc_lstm1_out)
-        enc_lstm3_out, (hn, cn) = self.enc_lstm3(enc_lstm2_out)
-
-        dec_embeddings = self.dec_embedding(None)
-        dec_lstm_out, _ = self.dec_lstm(dec_embeddings, (hn, cn))
-        fc_linear_out = self.fc_linear(dec_lstm_out)
-        output_layer_out = self.output_layer(fc_linear_out)
-
-        return output_layer_out
-
-    def sentence2tensor(self, sentence: str):
-        indexes = [self.vocabulary.index(word) for word in word_tokenize(sentence)]
-        return torch.tensor(indexes, dtype=torch.long)
-
-    def tensor2sentence(self, tensor: torch.Tensor):
-        predictions = []
-        for i, row in enumerate(tensor):
-            predictions.append(self.vocabulary[np.argmax(row)])
-
-        return ' '.join(predictions)
-
-    def train_model(self, train_data: List[str], references: List[str], epochs: int = 10) -> None:
-        loss = CrossEntropyLoss()
-        optimizer = RMSprop(self.parameters(), lr=self.learning_rate)
-
+        epoch_losses = []
         for epoch in range(epochs):
-            for article, reference in zip(train_data, references):
+            epoch_loss = 0
+            training_set = list(zip(train_data, references))
+            random.shuffle(training_set)
+            for article, reference in training_set:
+                sentence = []
+                loss = 0
+                encoder_hidden = self.encoder.init_hidden()
+
                 self.zero_grad()
+                self.encoder.zero_grad()
+                self.decoder.zero_grad()
 
                 inputs = self.sentence2tensor(article)
                 targets = self.sentence2tensor(reference)
 
-                outputs = self(inputs)
+                encoder_outputs, encoder_hidden = self.encoder(inputs, encoder_hidden)
 
-                loss_val = loss(outputs, targets)
-                loss_val.backward()
-                optimizer.step()
+                decoder_input = torch.tensor([[0]], dtype=torch.float)
+                decoder_hidden = encoder_hidden
 
-    def predict(self, data: List[str]) -> List[str]:
-        predictions = []
-        for sentence in data:
-            with torch.no_grad():
-                inputs = self.sentence2tensor(sentence)
-                scores = self(inputs)
-                predictions.append(self.tensor2sentence(scores))
+                for i in range(targets.shape[0]):
+                    decoder_output, decoder_hidden = self.decoder(decoder_input, decoder_hidden)
+                    topv, topi = decoder_output.topk(10)
+                    choice = random.randint(0, 9)
+                    decoder_input = torch.tensor([[topi[choice].squeeze().detach()]], dtype=torch.float)
 
-        return predictions
+                    if decoder_input.item() == 1 or decoder_input.item() == 0:
+                        break
+
+                    word = self.vector_model.wv.index2word[int(topi[choice]) - 2]
+                    if word not in self.vector_model.wv:
+                        word = "<UNK>"
+                        output_tensor = torch.zeros(self.embedding_size, dtype=torch.float)
+                    else:
+                        output_tensor = torch.tensor(self.vector_model.wv[word])
+
+                    sentence.append(word)
+
+                    loss += loss_criteria(output_tensor, targets[i])
+
+                epoch_loss += float(loss)
+                loss = loss.clone().detach().requires_grad_(True)
+                loss.backward()
+                encoder_optimizer.step()
+                decoder_optimizer.step()
+
+            epoch_loss /= len(train_data)
+            epoch_losses.append(epoch_loss)
+            print(f"Epoch {epoch} Loss: {epoch_loss}")
+            print(f"Last training example: \n")
+            print(f"Article: {' '.join(article)}")
+            print(f"Reference: {' '.join(reference)}")
+            print(f"Summary: {' '.join(sentence)}\n\n\n")
+
+        return epoch_losses
+
+    def predict(self, data: List[str]) -> List[List[str]]:
+        output_sentences = []
+
+        for article in data:
+            sentence = []
+            inputs = self.sentence2tensor(article)
+
+            encoder_outputs, encoder_hidden = self.encoder(inputs)
+
+            decoder_input = torch.tensor([[0]], dtype=torch.float)
+            decoder_hidden = encoder_hidden
+
+            for i in range(self.max_length):
+                decoder_output, decoder_hidden = self.decoder(decoder_input, decoder_hidden)
+                topv, topi = decoder_output.topk(10)
+                choice = random.randint(0, 9)
+                decoder_input = torch.tensor([[topi[choice].squeeze().detach()]], dtype=torch.float)
+
+                word = self.vector_model.wv.index2word[int(topi[choice])]
+                if word not in self.vector_model.wv:
+                    word = "<UNK>"
+                sentence.append(word)
+
+                if decoder_input.item() == 1:
+                    break
+
+            output_sentences.append(sentence)
+
+        return output_sentences
+
+    def save(self):
+        torch.save(self.encoder.state_dict(), "encoder.pt")
+        torch.save(self.decoder.state_dict(), "decoder.pt")
